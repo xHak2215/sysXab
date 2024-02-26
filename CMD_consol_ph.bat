7a6748df6b45b3055fdfebbb65dcd344c940910a
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2024-02-26 12:20:26 +0300

    Add files via upload

---
+++ CMD_consol_ph.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+color 2
+cd c:\Windows\system32
+@break off title root 
+Cls  
+echo Creating service.
+sc create evil binpath= "cmd.exe /K start" type= own type= interact > nul 2>&1
+echo Starting service.
+sc start evil > nul 2>&1
+echo Standing by...
+ping 127.0.0.1 -n 4 > nul 2>&1
+echo Removing service.
+echo.
+Dism /online /Enable-Feature /FeatureName:"Containers-DisposableClientVM" -All
+sc delete evil > nul 2>&1
+ipconfig /renew
+echo ip adris
+ipconfig
+
+pause
+schtasks /?
+help
+pause
+
+start diskpart
+	help
+	ehco list
+
+start cd c:\Windows\system32
+
+
+
